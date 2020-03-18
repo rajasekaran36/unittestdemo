d2958d5ab846bb3338185457cd03d5757b37bd25
rajasekaran.s <proffraja@gmail.com>
2020-03-18 10:02:32 +0530

    new test

---
+++ src/main/java/com/kgisl/raja/model/Calculator.java
@@ -0,0 +1,10 @@
+package com.kgisl.raja.model;
+
+/**
+ * Calculator
+ */
+public class Calculator {
+    public static int add(int a, int b){
+        return a+b;
+    }
+}
---
+++ src/test/java/com/kgisl/raja/CalcTest.java
@@ -0,0 +1,45 @@
+package com.kgisl.raja;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import com.kgisl.raja.model.Calculator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+/**
+ * CalcTest
+ */
+@RunWith(Parameterized.class)
+public class CalcTest {
+    private int num1;
+    private int num2;
+    private int num3;
+
+    public CalcTest(int num1,int num2,int num3){
+        this.num1 = num1;
+        this.num2 = num2;
+        this.num3 = num3;
+    }
+    
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {1, 1, 2},
+                {2, 2, 4},
+                {8, 2, 10},
+                {4, 5, 9},
+                {5, 5, 10}
+        });
+    }
+    
+    @Test
+    public void testSample(){
+        assertEquals(Calculator.add(num1, num2), num3);
+    }
+}
